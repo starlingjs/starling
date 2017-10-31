d6cc139acdc7893e92424d3a167d7eb2719be990
Nick Fitzgerald <fitzgen@gmail.com>
2017-10-31 15:05:15 -0700

    Typo: "execut" -> "execute"

--- tests/js/timeout-4.js
+++ tests/js/timeout-4.js
@@ -1,7 +1,7 @@
 // The task stops after `main` exits and then we flush the micro-task queue. Any
 // promise that hasn't settled is abandoned. Therefore, the error after the
 // timeout will not trigger our unhandled-rejected-promises task killing because
-// it will never execut and throw.
+// it will never execute and throw.
 
 async function main() {
   // No await.
